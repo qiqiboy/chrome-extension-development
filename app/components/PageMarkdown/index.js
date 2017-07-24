c7772ba6b3526151e8062ec758c90494fca0a1b4
qiqiboy <imqiqiboy@gmail.com>
2017-07-24 11:29:17 +0800

    切换markdown tab不自动打开预览
    Signed-off-by: qiqiboy <imqiqiboy@gmail.com>

--- app/components/PageMarkdown/index.js
+++ app/components/PageMarkdown/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import marked from 'marked';
 import * as mdUtil from '../../utils/mdUtil';
+import OptionItem from '../../components/OptionItem';
 import './style.scss';
 
 const chrome = window.chrome;
 
 class Markdown extends Component {
+    state = {}
+
     async componentDidMount() {
         const [Editor] = await Promise.all([
             import('codemirror'), //异步载入编辑器代码
@@ -22,8 +25,6 @@ class Markdown extends Component {
         });
 
         this.editor.on('change', this.preview);
-
-        this.preview();
     }
 
     preview = () => {
@@ -48,10 +49,19 @@ class Markdown extends Component {
         });
     }
 
+    openPreview = clicked => {
+        this.preview();
+
+        this.setState({
+            clicked
+        });
+    }
+
     render() {
 
         return (
             <div className="markdown">
+                <OptionItem active={this.state.clicked} onChange={this.openPreview}>直接开始编辑，或者点此打开预览页面</OptionItem>
                 <div className="md-editor" ref="editor"></div>
                 <button className="btn btn-success" onClick={this.splitScreen}>分屏编辑模式</button>
                 <div className="readme">注：分屏模式将会打开两个并排窗口，左边是文档预览，右边是编辑器。</div>
